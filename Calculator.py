2cc18144785f2370d077604aa64c8c1cdddbf8a3
lavenderbonnet <52668836+lavenderbonnet@users.noreply.github.com>
2019-07-08 11:45:47 -0500

    Finished Tests

--- Calculator.py
+++ Calculator.py
@@ -23,20 +23,43 @@ def Div(dividend,divisor):
     
 # ---------------------------------------------------------------------------------------------------------------
 # Tests
-# Addition
 
-Ans = Add(256978,349687)
-print (Ans)
+leftOp = 256978
+rightOp = 349687
 
-Ans = Add(2,3)
-print (Ans)
+rAdd = Add(leftOp,rightOp)
+rSub = Sub(leftOp,rightOp)
+rMul = Mul(leftOp,rightOp)
+rDiv = Div(leftOp,rightOp)
+print (rAdd, rSub, rMul, rDiv)
 
-Ans = Add(123456789009123456745678944545680,234567829578436756789639786789)
-print (Ans)
+leftOp = 2
+rightOp = 3
 
+rAdd = Add(leftOp,rightOp)
+rSub = Sub(leftOp,rightOp)
+rMul = Mul(leftOp,rightOp)
+rDiv = Div(leftOp,rightOp)
+print (rAdd, rSub, rMul, rDiv)
 
-Ans = Add(123456789009123456745678947663467587698790778764565342765987907097889678574654545680,1)
-print (Ans)
+leftOp = 123456789009123456745678944545680
+rightOp = 234567829578436756789639786789
 
+rAdd = Add(leftOp,rightOp)
+rSub = Sub(leftOp,rightOp)
+rMul = Mul(leftOp,rightOp)
+rDiv = Div(leftOp,rightOp)
+print (rAdd, rSub, rMul, rDiv)
 
+leftOp =123456789009123456745678947663467587698790778764565342765987907097889678574654545680
+rightOp = 1
+
+rAdd = Add(leftOp,rightOp)
+rSub = Sub(leftOp,rightOp)
+rMul = Mul(leftOp,rightOp)
+rDiv = Div(leftOp,rightOp)
+print (rAdd, rSub, rMul, rDiv)
+
+# Special Test by zero
+print ( Div(1,0) )
 #---------------------------------------------------------------------------------------------------------------
